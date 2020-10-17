c5ab51c13adcbd1b75e54432f5b9611b367f430c
Jake Alexander <jalexander91196@gmail.com>
2020-10-17 16:27:07 -0400

    media changes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,45 +5,28 @@ import waterbg from '../images/waves_05.mp4';
 export default function Homepage() {
     return (
         <>
-            <Card>
+            
                 <div className="jumbotron">
                     <h1>Rachel and Oliver</h1>    
                     <h3>November 11th, 2021 -- Key West, Flordia</h3>
                 </div>
-            </Card>
+           
                 <div className="main">
-            <Card>
+            
                 <div className="note-container">
-                            <span className="intro1">
-                                <p>
+                           
+                                <p className="note-intro">
                                     Dear Family and Friends, <br /> <br />
-                                </p>
-                            </span>
-                            <span className="intro2">
-                                <p>
                                     We are delighted to share with you this amazingly beautiful and special moment in our lives;
                                     the celebration of our love! <br /> <br />
-                                </p>
-                            </span>
-                            <span className="intro2">
-                                <p>
                                     In order to prepare you for the big day, we have created this site where all of our wedding information will be shared. <br /> <br />
-                                </p>
-                            </span>
-                            <span className="intro2">
-                                <p>
                                     We can't wait, <br /> <br />
                                     See you in Florida!! <br /> <br />
-                                </p>
-                            </span>
-                            <span className="intro3">
-                                <p>
                                     All Love, <br/>
                                     Rachel and Oliver 
                                 </p>
-                            </span>
-                </div>   
-            </Card>  
+                            
+                </div>    
                     <div className="water-bg">
                         <video className='videoTag' autoPlay loop muted>
                             <source src={waterbg} type='video/mp4' />
